dd8976ee7625d03ca5d27f5e8cb52f0b46f837c9
PStuart <patrick.stuart@live.com.au>
2024-07-14 15:39:02 +1000

    Function

---
+++ frontend/functions/api.js
@@ -0,0 +1,46 @@
+import express, { Router } from "express";
+import serverless from "serverless-http";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const api = express();
+
+const router = Router();
+
+const jsonParser = bodyParser.json();
+
+router.post(('/search', jsonParser, async (req,res) => {
+    try {
+        let payload = req.body;
+
+        let title = payload.title;
+
+        const options = {
+            method: 'GET',
+            url: 'https://streaming-availability.p.rapidapi.com/shows/search/title',
+            params: {
+              country: 'au',
+              title: title,
+              output_language: 'en',
+              show_type: 'movie',
+              series_granularity: 'show'
+            },
+            headers: {
+              'X-RapidAPI-Key': fs.readFileSync('key.txt').toString(),
+              'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+            }
+        }
+
+        const response = await axios.request(options);
+        res.status(200).send(response.data);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send()
+    }
+
+
+}))
+
+api.use("/api/", router);
+export const handler = serverless(api);
